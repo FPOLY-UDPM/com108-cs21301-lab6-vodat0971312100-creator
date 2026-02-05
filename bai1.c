e56968e98ff761cdc5d5ab907c7a6d0fd1af012d
vodat0971312100 <vodat0971312100@gmail.com>
2026-02-05 08:48:39 +0700

    Minh Đạt nộp lab 6

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Võ Minh Đạt]
+ * MSSV:      [Ps49299]
+ * Lớp:       [COm108]
  *****************************************************************************/
 
 //  BÀI 1: TÍNH TRUNG BÌNH TỔNG CÁC SỐ CHIA HẾT CHO 3 TRONG MẢNG 
@@ -9,5 +9,42 @@
 //  Output: Xuất ra màn hình kết quả của trung bình tổng các số chia hết cho 3 trong mảng 
 
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+int main(){
+    int i,n;
+    printf("xin nhap vao n:");
+    scanf("%d",&n);
+    int arr_1[n];
+   for(i=0;i<n;i++) {
+        printf("xin mời nhập vào số %d: ",i);
+        scanf("%d",&arr_1[i]);
+    }
+    float tong=0;
+    float Tb=0;
+    int count;
+    for(i=0;i<n;i++){
+        if(arr_1[i]%2!=0){
+            tong+= arr_1[i];
+            count++;
+        }
+    }
+    Tb=tong/count;
+    printf("vậy kết quả là %1f",Tb);
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
