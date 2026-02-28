d05347b1b6e0f97cb619f494451400ba74476f78
vodat0971312100 <vodat0971312100@gmail.com>
2026-02-28 08:35:59 +0700

    lab6

--- bai4.c
+++ bai4.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Võ Minh Đạt ]
+ * MSSV:      [PS49299]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
@@ -9,4 +9,33 @@
 //  Output: Xuất ra màn hình ma trận bình phương 
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
 
+int main() {
+    int n, m;
+    printf("Nhap so hang: ");
+    scanf("%d", &n);
+    printf("Nhap so cot: ");
+    scanf("%d", &m);
+
+    int a[100][100]; // giả sử tối đa 100x100
+
+    // Nhap ma tran
+    printf("Nhap cac phan tu cua ma tran:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Xuat ma tran binh phuong
+    printf("Ma tran binh phuong:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", a[i][j] * a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
